67634454955c9b54d9c1f285a09c48fb15789d16
virtualLantner <xlunter@stuba.sk>
2020-12-25 17:58:09 +0100

    bredscrump script added

---
+++ assets/js/scripts_breadcrumb.js
@@ -0,0 +1,62 @@
+$( document ).ready(function() {
+
+    var breadcrumbArr = [];
+    var href = document.location.href;
+    var splitHref = href.split("/");
+    
+    // if cookies not set -> set them ... else get and change
+    if ( getCookie('breadcrumbs') == "" ) 
+    {
+      breadcrumbArr.push(splitHref[splitHref.length -1])
+      setCookie("breadcrumbs", breadcrumbArr, 1)
+      
+    } else 
+    {
+      var breadcrumbArr = getCookie('breadcrumbs').split(',');
+
+      // only 5 last visited pages
+      if ( breadcrumbArr.length == 5 ) {
+        breadcrumbArr.shift();
+      }
+
+      breadcrumbArr.push(splitHref[splitHref.length-1])
+      setCookie("breadcrumbs", breadcrumbArr, 1)
+    }
+    
+    
+  
+    var breadcrumbEl = $('#breadcrumbNav')
+    for (let i = 0; i < 5; i++) {
+      if ( i == 4 ) {
+        breadcrumbEl.append('<li><a href="'+ arr[i] +'">'+arr[i]+'</a></li>')
+      } else {
+        breadcrumbEl.append('<li><a href="'+ arr[i] +'">'+arr[i]+'</a> > </li>')
+      }
+    }
+  
+  
+
+    // Cookies
+    // https://www.w3schools.com/js/js_cookies.asp
+    function setCookie(cname, cvalue, exdays) {
+      var d = new Date();
+      d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+      var expires = "expires="+d.toUTCString();
+      document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+  
+    function getCookie(cname) {
+      var name = cname + "=";
+      var ca = document.cookie.split(';');
+      for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+        }
+      }
+      return "";
+    }
+  })
