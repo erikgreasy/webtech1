dd790bd6bd3c5db09e848741349f7defed7ffeb6
butora.roman1@gmail.com <butora.roman1@gmail.com>
2020-12-30 15:34:24 +0100

    path changed

--- assets/js/scripts_nameDayComponent.js
+++ assets/js/scripts_nameDayComponent.js
@@ -56,7 +56,7 @@ function toggleSearch(){
     }
 }
 
-var xmlFile = String(window.location.href).replace('/#',"") + '/assets/meniny.xml';
+var xmlFile = './assets/meniny.xml';;
 var xmlDoc; var skName; var skSviatok;
 
 function loadDoc() {
